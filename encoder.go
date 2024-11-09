2583ed95156ceba5ce8f5da73d79569fd744e7c8
shellphy <ai.shellphy@gmail.com>
2024-11-10 01:20:05 +0800

    增加插件名称固定长度

--- encoder.go
+++ encoder.go
@@ -26,7 +26,7 @@ func ColoredLevelEncoder(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder)
 // ColoredNameEncoder colorizes service names.
 func ColoredNameEncoder(s string, enc zapcore.PrimitiveArrayEncoder) {
 	if len(s) < 20 {
-		s += strings.Repeat(" ", 12-len(s))
+		s += strings.Repeat(" ", 20-len(s))
 	}
 
 	enc.AppendString(color.HiGreenString(s))
